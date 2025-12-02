a396cc375e0b12588da665f5cb37ee814700e2af
cjy <cjy3380@gmail.com>
2025-12-02 14:07:49 +0900

    finish

---
+++ 2d_ptr.c
@@ -0,0 +1,16 @@
+﻿// 2d_ptr.c
+#include <stdio.h>
+
+int main(void) {
+	short data = 3,
+		* p = &data, // data 변수의 주소
+		** p = &p; // 포인터 p의 주소	
+
+	printf("[Before] data: %d\n", data); // 3
+	*p = 40;
+	printf("[Use *p] data: %d\n", data); // 40
+	**p = 500;
+	printf("[Use **p] data: %d\n", data); // 500
+
+	return 0;	
+}
---
+++ malloc_arr.c
@@ -0,0 +1,37 @@
+﻿// malloc_arr.c
+// mem_arr.c
+#include <stdio.h>
+#include <malloc.h>
+
+int main(void) {
+	int* p_nums_list,
+		count = 0,
+		sum = 0,
+		limit = 0,
+		i;
+
+	printf("사용할 최대 개수를 입력하세요: >>> ");
+	scanf_s("%d", &limit);
+
+	p_nums_list = (int*)malloc(sizeof(int) * limit);	
+
+	while (count < MAX_COUNT) {
+		printf("숫자를 입력하세요 (9999를 누르면 종료): ");
+		scanf_s("%d", p_nums_list + count);
+
+		if (*(p_nums_list + count) == 9999) break;
+
+		count++; // 입력된 횟수를 계산함
+	}
+
+	// 입력된 횟수만큼 숫자를 출력함
+	for (i = 0; i < count; i++) {
+		if (i > 0) printf("+"); 
+		printf("%d", *(p_nums_list + i)); // 변경
+		sum = sum + *(p_nums_list + i); // 변경	
+	}
+	printf("= %d\n", sum);
+	free(p_nums_list); // 사용했던 메모리를 제거함
+
+	return 0;
+}
